de809730c5f1c81a16ddfce4b5dcdc44226b7871
Pavle Neskovic <pavlenesko@gmail.com>
2016-08-15 10:20:25 +0200

    pre uvodjenja novog api-ja

---
+++ src/main/java/com/example/actuator/health/UsersHealthIndicator.java
@@ -0,0 +1,32 @@
+package com.example.actuator.health;
+
+import java.util.Collection;
+
+import org.apache.tomcat.jni.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+import com.example.dto.UserDto;
+import com.example.service.UserService;
+
+@Component
+public class UsersHealthIndicator implements HealthIndicator {
+
+	@Autowired
+	private UserService userService;
+	
+	@Override
+	public Health health() {
+		Collection<UserDto> users = userService.getAll();
+		
+		//Ako se ne podize server uspesno, mozda je prazna baza, pa ulazi u if
+		if (users == null || users.size() == 0) {
+			System.err.println("Server is down, because there is no users in db.");
+			return Health.down().withDetail("count", users.size()).build();
+		}
+		return Health.up().withDetail("count",users.size()).build();
+	}
+
+}
