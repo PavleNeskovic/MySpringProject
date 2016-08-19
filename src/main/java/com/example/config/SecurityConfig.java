6f6f4fe261087b60bd4707608c81448e6a2e4756
Pavle Neskovic <pavlenesko@gmail.com>
2016-08-19 12:29:12 +0200

    disabled csfr

--- src/main/java/com/example/config/SecurityConfig.java
+++ src/main/java/com/example/config/SecurityConfig.java
@@ -3,16 +3,49 @@ package com.example.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.configuration.*;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+	  @Autowired
+	    private UserDetailsService userDetailsService;
+	  
+	  @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	  /*http.formLogin()
+        .loginPage("/login")
+        .usernameParameter("email")
+        .permitAll()
+        .and()
+        .logout()
+        .logoutUrl("/logout")
+        .permitAll();*/
+	http.authorizeRequests().anyRequest().authenticated().and().httpBasic().and().
+	csrf().disable();
+	/* http.authorizeRequests()
+	.antMatchers("/users").hasRole("ADMIN") // #4
+	        .antMatchers("/messages").hasRole("ADMIN") // #6
+	        .antMatchers("/messages/send").hasRole("USER") 
+	        .anyRequest().authenticated() // 7
+	        .and()
+	    .formLogin() 
+	    .and().httpBasic().and().
+	csrf().disable();*/
+
+	  }
+
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth
-            .inMemoryAuthentication()
-                .withUser("a").password("a").roles("USER");
+//            .inMemoryAuthentication()
+//                .withUser("a").password("a").roles("USER");
+		        .userDetailsService(userDetailsService)
+		        .passwordEncoder(new BCryptPasswordEncoder());
     }
 }
---
+++ src/main/java/com/example/service/CurrentUserDetailsService.java
@@ -0,0 +1,26 @@
+package com.example.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.example.model.CurrentUser;
+import com.example.model.User;
+
+@Service
+public class CurrentUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Autowired
+    public CurrentUserDetailsService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public CurrentUser loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.getUserByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException(String.format("User with email=%s was not found", email)));
+        return new CurrentUser(user);
+    }
+}
